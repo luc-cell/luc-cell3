ffff2c6b82dcd0a1d7a6c41ee3a5c409fafcad7b
luc-cell <62962336+luc-cell@users.noreply.github.com>
2020-03-31 22:24:01 +0200

    Add files via upload

---
+++ the two timer.py
@@ -0,0 +1,15 @@
+import time
+
+
+fuck = ("7777777777777777777777777777777777777777")
+cock = ("YEP_COCK")
+twotimer = ("forsenCD")
+Any_two_timers = ("Any_two_timers_?_?_?")
+Bad_Mistakes_MaN = ("Bad_Mistakes_MaN")
+while True:
+  print(Bad_Mistakes_MaN)
+  print(Any_two_timers)
+  print(twotimer)
+  print(cock)
+  print(fuck)
+  time.sleep(0.09)
